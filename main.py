f08e55b2c73eef6767acd9072190f61808f70f9a
MannanAi <162859287+MannanAi@users.noreply.github.com>
2024-03-10 10:42:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pyrogram input Client, filters
 
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+
+MannanAiTechnologies = Client(
+   name= "MayilBot",
+   api_id=API_ID,
+   api_hash=API_HASH,
+   bot_token=BOT_TOKEN
+)
+
+
+print("Mannan's Mayil Bot Was Started Working")
+
+MannanAiTechnologies.run()
